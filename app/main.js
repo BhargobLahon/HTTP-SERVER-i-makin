dd3c16bf3125615c0c04436b62b6f891098ecf34
BhargobLahon <lahonbhargab@gmail.com>
2024-06-22 23:53:49 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ const conn = new Socket;
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
-const server = net.createServer((conn) => {
+const server = net.createServer((socket) => {
   conn.write("HTTP/1.1 200 OK\r\n\r\n");
   conn.on("close", () => {
     conn.end();
