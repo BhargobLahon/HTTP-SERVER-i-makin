84a3c47d2dad956706e661bf9eef8e1e5254c79a
BhargobLahon <lahonbhargab@gmail.com>
2024-06-22 23:53:08 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,13 +1,14 @@
+const { Socket } = require("dgram");
 const net = require("net");
-
+const conn = new Socket;
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
-const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
-  socket.on("close", () => {
-    socket.end();
+const server = net.createServer((conn) => {
+  conn.write("HTTP/1.1 200 OK\r\n\r\n");
+  conn.on("close", () => {
+    conn.end();
     server.close();
   });
 });
